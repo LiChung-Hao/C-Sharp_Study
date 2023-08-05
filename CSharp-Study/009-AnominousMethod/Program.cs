9c08b8e67af113e129042c1c2a78f1e4bcbb256e
Li, ChungHao <wesley.lee.hau@gmail.com>
2023-08-05 15:55:02 -0400

    Studied anominous method

---
+++ CSharp-Study/009-AnominousMethod/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _009_AnominousMethod
+{
+    internal class Program
+    {
+        static int Test1(int a, int b)
+        { 
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            //Func<int, int, int> Add = Test1; //normal way
+
+            Func<int, int, int> Add = delegate (int a, int b) //anominous way
+            {//any delegate can use anominous method to give it a value
+                return (a + b);
+            };
+        }
+    }
+}
